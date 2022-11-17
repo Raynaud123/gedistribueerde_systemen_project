a41c3cd27265e3e26abd52935b3f58aaa47e1e99
raynaud <raynaud.cornille@gmail.com>
2022-11-17 10:04:52 +0100

    Data in matching service is deleted after predefined interval(in this case 7 days)

--- MatchingService/src/main/java/com/example/matchingservice/MatchingService.java
+++ MatchingService/src/main/java/com/example/matchingservice/MatchingService.java
@@ -1,14 +1,27 @@
 package com.example.matchingservice;
 
+import java.rmi.RemoteException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MatchingService {
-    private final ArrayList<Capsule> capsules;
+    private ArrayList<Capsule> capsules;
 
 
     public MatchingService() {
         this.capsules = new ArrayList<>();
+        // Capsules need to be deleted after predefined interval(in this case 7 days)
+        Timer timer = new Timer ();
+        TimerTask weeklyTask = new TimerTask () {
+            @Override
+            public void run () {
+                capsules = new ArrayList<>();
+            }
+        };
+        timer.schedule (weeklyTask, 0L, 7*24*1000*60*60);
     }
 
     public void addCapsule(String hex, Timestamp timeInterval, String userToken) {
